6ed2b272ac2ef26bf39bf0149d5badcad2f37c26
Max Martin <maxmart97@gmail.com>
2018-10-25 23:39:30 -0500

    Added Project

---
+++ If Demo/If Demo/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace If_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpaPromptLabel = new System.Windows.Forms.Label();
+            this.gpaTextBox = new System.Windows.Forms.TextBox();
+            this.honorRollLabel = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.scorePromptLabel = new System.Windows.Forms.Label();
+            this.scoreTextBox = new System.Windows.Forms.TextBox();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.gradeOutputDescriptionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gpaPromptLabel
+            // 
+            this.gpaPromptLabel.AutoSize = true;
+            this.gpaPromptLabel.Location = new System.Drawing.Point(137, 33);
+            this.gpaPromptLabel.Name = "gpaPromptLabel";
+            this.gpaPromptLabel.Size = new System.Drawing.Size(37, 17);
+            this.gpaPromptLabel.TabIndex = 0;
+            this.gpaPromptLabel.Text = "GPA";
+            // 
+            // gpaTextBox
+            // 
+            this.gpaTextBox.Location = new System.Drawing.Point(104, 72);
+            this.gpaTextBox.Name = "gpaTextBox";
+            this.gpaTextBox.Size = new System.Drawing.Size(100, 22);
+            this.gpaTextBox.TabIndex = 1;
+            // 
+            // honorRollLabel
+            // 
+            this.honorRollLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.honorRollLabel.Location = new System.Drawing.Point(132, 130);
+            this.honorRollLabel.Name = "honorRollLabel";
+            this.honorRollLabel.Size = new System.Drawing.Size(182, 23);
+            this.honorRollLabel.TabIndex = 6;
+            this.honorRollLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(61, 238);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(100, 32);
+            this.processButton.TabIndex = 3;
+            this.processButton.Text = "&Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(199, 238);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 32);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "&Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(332, 238);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 32);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // scorePromptLabel
+            // 
+            this.scorePromptLabel.AutoSize = true;
+            this.scorePromptLabel.Location = new System.Drawing.Point(269, 33);
+            this.scorePromptLabel.Name = "scorePromptLabel";
+            this.scorePromptLabel.Size = new System.Drawing.Size(45, 17);
+            this.scorePromptLabel.TabIndex = 7;
+            this.scorePromptLabel.Text = "Score";
+            // 
+            // scoreTextBox
+            // 
+            this.scoreTextBox.Location = new System.Drawing.Point(241, 72);
+            this.scoreTextBox.Name = "scoreTextBox";
+            this.scoreTextBox.Size = new System.Drawing.Size(100, 22);
+            this.scoreTextBox.TabIndex = 2;
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gradeLabel.Location = new System.Drawing.Point(421, 124);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(45, 29);
+            this.gradeLabel.TabIndex = 9;
+            this.gradeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gradeOutputDescriptionLabel
+            // 
+            this.gradeOutputDescriptionLabel.AutoSize = true;
+            this.gradeOutputDescriptionLabel.Location = new System.Drawing.Point(367, 130);
+            this.gradeOutputDescriptionLabel.Name = "gradeOutputDescriptionLabel";
+            this.gradeOutputDescriptionLabel.Size = new System.Drawing.Size(48, 17);
+            this.gradeOutputDescriptionLabel.TabIndex = 10;
+            this.gradeOutputDescriptionLabel.Text = "Grade";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.processButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(516, 339);
+            this.Controls.Add(this.gradeOutputDescriptionLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.scoreTextBox);
+            this.Controls.Add(this.scorePromptLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.honorRollLabel);
+            this.Controls.Add(this.gpaTextBox);
+            this.Controls.Add(this.gpaPromptLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "If Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gpaPromptLabel;
+        private System.Windows.Forms.TextBox gpaTextBox;
+        private System.Windows.Forms.Label honorRollLabel;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label scorePromptLabel;
+        private System.Windows.Forms.TextBox scoreTextBox;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label gradeOutputDescriptionLabel;
+    }
+}
+
---
+++ If Demo/If Demo/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            double gpa;
+            double score;
+
+            if (double.TryParse(gpaTextBox.Text, out gpa) && double.TryParse(scoreTextBox.Text, out score))
+            {
+                gpa = Math.Round(gpa, 2);
+                score = Math.Round(score, 2);
+
+                gpaTextBox.Text = gpa.ToString("n2");
+                scoreTextBox.Text = score.ToString("n2");
+
+                if ((gpa >= 0.00 && gpa <= 4.00) && (score >= 0.00 && score <= 100.00))
+                {
+                    if (gpa >= 3.50)
+                    {
+                        honorRollLabel.Text = "Honor Roll";
+                    }
+                    else if (gpa < 2.50)
+                    {
+                        honorRollLabel.Text = "P R O B A T I O N";
+                    }
+                    else
+                    {
+                        honorRollLabel.Text = "Tootsie Roll";
+                    }
+
+                    if (score >= 90.0)
+                    {
+                        gradeLabel.Text = "A";
+                    }
+                    else if (score >= 80.0 && score < 90.0)
+                    {
+                        gradeLabel.Text = "B";
+                    }
+                    else if (score >= 70.0 && score < 80.0)
+                    {
+                        gradeLabel.Text = "C";
+                    }
+                    else if (score >= 60.0 && score < 70.0)
+                    {
+                        gradeLabel.Text = "D";
+                    }
+                    else
+                    {
+                        gradeLabel.Text = "F";
+                    }
+
+                }
+                else if ((gpa < 0.00 || gpa > 4.00) && (score >= 0.00 && score <= 100.00))
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00).");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+
+                    gpaTextBox.Focus();
+                }
+                else if ((gpa >= 0.00 && gpa <= 4.00) && (score < 0.00 || score > 100.00))
+                {
+                    MessageBox.Show("Please enter a score between (0.00-100.00).");
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+
+                    scoreTextBox.Focus();
+                }
+                else if ((gpa < 0.00 || gpa > 4.00) && (score < 0.00 || score > 100.00))
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00) and/or a score between (0.00-100.00).");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+
+                    gpaTextBox.Focus();
+                }
+            }
+            else if (double.TryParse(gpaTextBox.Text, out gpa) && !double.TryParse(scoreTextBox.Text, out score))
+            {
+                gpa = Math.Round(gpa, 2);
+
+                gpaTextBox.Text = gpa.ToString("n2");
+
+                if ((gpa >= 0.00 && gpa <= 4.00) && (scoreTextBox.Text == ""))
+                {
+                    if (gpa >= 3.50)
+                    {
+                        honorRollLabel.Text = "Honor Roll";
+                    }
+                    else if (gpa < 2.50)
+                    {
+                        honorRollLabel.Text = "P R O B A T I O N";
+                    }
+                    else
+                    {
+                        honorRollLabel.Text = "Tootsie Roll";
+                    }
+
+                    gradeLabel.Text = "";
+                }
+                else if ((gpa >= 0.00 && gpa <= 4.00) && scoreTextBox.Text != "")
+                {
+                    MessageBox.Show("Please enter a score between (0.00-100.00) or leave it blank.");
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+                    scoreTextBox.Focus();
+                }
+                else if ((gpa < 0.00 || gpa > 4.00) && scoreTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00).");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+                    gpaTextBox.Focus();
+                }
+                else if ((gpa < 0.00 || gpa > 4.00) && scoreTextBox.Text != "")
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00) and/or a score between (0.00-100.00).");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+
+                    gpaTextBox.Focus();
+                }
+            }
+            else if (!double.TryParse(gpaTextBox.Text, out gpa) && double.TryParse(scoreTextBox.Text, out score))
+            {
+                score = Math.Round(score, 2);
+
+                scoreTextBox.Text = score.ToString("n2");
+
+                if ((gpaTextBox.Text == "") && (score >= 0.00 && score <= 100.00))
+                {
+                    if (score >= 90.0)
+                    {
+                        gradeLabel.Text = "A";
+                    }
+                    else if (score >= 80.0 && score < 90.0)
+                    {
+                        gradeLabel.Text = "B";
+                    }
+                    else if (score >= 70.0 && score < 80.0)
+                    {
+                        gradeLabel.Text = "C";
+                    }
+                    else if (score >= 60.0 && score < 70.0)
+                    {
+                        gradeLabel.Text = "D";
+                    }
+                    else
+                    {
+                        gradeLabel.Text = "F";
+                    }
+
+                    honorRollLabel.Text = "";
+                }
+                else if ((gpaTextBox.Text != "") && (score >= 0.00 && score <= 100.00))
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00) or leave it blank.");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+                    gpaTextBox.Focus();
+                }
+                else if ((gpaTextBox.Text == "") && (score < 0.00 || score > 100.00))
+                {
+                    MessageBox.Show("Please enter a score between (0.00-100.00).");
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+                    scoreTextBox.Focus();
+                }
+                else if ((gpaTextBox.Text != "") && (score < 0.00 || score > 100.00))
+                {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00) and/or a score between (0.00-100.00).");
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+
+                    gpaTextBox.Focus();
+                }
+            }
+            else
+            {
+                    MessageBox.Show("Please enter a GPA between (0.00-4.00) and/or a score between (0.00-100.00).");
+
+                    honorRollLabel.Text = "";
+                    gradeLabel.Text = "";
+
+                    gpaTextBox.Clear();
+                    honorRollLabel.Text = "";
+
+                    scoreTextBox.Clear();
+                    gradeLabel.Text = "";
+
+                    gpaTextBox.Focus();
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            gpaTextBox.Clear();
+            honorRollLabel.Text = "";
+
+            scoreTextBox.Clear();
+            gradeLabel.Text = "";
+
+            gpaTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
