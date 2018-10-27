ba7cfccd4274fa68da16881b436d22bf64d98334
Max Martin <maxmart97@gmail.com>
2018-10-26 19:12:49 -0500

    Whole Project

---
+++ Looping/Looping/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Looping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListbox = new System.Windows.Forms.ListBox();
+            this.startButtonWhileLoop = new System.Windows.Forms.Button();
+            this.startButtonDoWhileLoop = new System.Windows.Forms.Button();
+            this.startButtonForLoop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListbox
+            // 
+            this.outputListbox.FormattingEnabled = true;
+            this.outputListbox.ItemHeight = 16;
+            this.outputListbox.Location = new System.Drawing.Point(420, 39);
+            this.outputListbox.Name = "outputListbox";
+            this.outputListbox.Size = new System.Drawing.Size(305, 468);
+            this.outputListbox.TabIndex = 0;
+            // 
+            // startButtonWhileLoop
+            // 
+            this.startButtonWhileLoop.Location = new System.Drawing.Point(101, 61);
+            this.startButtonWhileLoop.Name = "startButtonWhileLoop";
+            this.startButtonWhileLoop.Size = new System.Drawing.Size(212, 101);
+            this.startButtonWhileLoop.TabIndex = 1;
+            this.startButtonWhileLoop.Text = "Start (While Loop)";
+            this.startButtonWhileLoop.UseVisualStyleBackColor = true;
+            this.startButtonWhileLoop.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // startButtonDoWhileLoop
+            // 
+            this.startButtonDoWhileLoop.Location = new System.Drawing.Point(101, 233);
+            this.startButtonDoWhileLoop.Name = "startButtonDoWhileLoop";
+            this.startButtonDoWhileLoop.Size = new System.Drawing.Size(212, 101);
+            this.startButtonDoWhileLoop.TabIndex = 2;
+            this.startButtonDoWhileLoop.Text = "Start (Do-While Loop)";
+            this.startButtonDoWhileLoop.UseVisualStyleBackColor = true;
+            this.startButtonDoWhileLoop.Click += new System.EventHandler(this.startButtonDoWhileLoop_Click);
+            // 
+            // startButtonForLoop
+            // 
+            this.startButtonForLoop.Location = new System.Drawing.Point(101, 406);
+            this.startButtonForLoop.Name = "startButtonForLoop";
+            this.startButtonForLoop.Size = new System.Drawing.Size(212, 101);
+            this.startButtonForLoop.TabIndex = 3;
+            this.startButtonForLoop.Text = "Start (For Loop)";
+            this.startButtonForLoop.UseVisualStyleBackColor = true;
+            this.startButtonForLoop.Click += new System.EventHandler(this.startButtonForLoop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 590);
+            this.Controls.Add(this.startButtonForLoop);
+            this.Controls.Add(this.startButtonDoWhileLoop);
+            this.Controls.Add(this.startButtonWhileLoop);
+            this.Controls.Add(this.outputListbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListbox;
+        private System.Windows.Forms.Button startButtonWhileLoop;
+        private System.Windows.Forms.Button startButtonDoWhileLoop;
+        private System.Windows.Forms.Button startButtonForLoop;
+    }
+}
+
---
+++ Looping/Looping/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Looping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //While Loop Example
+
+            int i = 1;
+
+            while (i < 5)
+            {
+                outputListbox.Items.Add("WHILE LOOP ITERATION: " + i);
+
+                i++;
+            }
+        }
+
+        private void startButtonDoWhileLoop_Click(object sender, EventArgs e)
+        {
+            //Do-While Loop Example
+
+            int i = 10;
+
+            do
+            {
+                outputListbox.Items.Add("DO-WHILE LOOP ITERATION: " + i);
+                MessageBox.Show("Infinite loop " + i);
+                i++;
+            }
+            while (i < 16);
+        }
+
+        private void startButtonForLoop_Click(object sender, EventArgs e)
+        {
+            //For Loop Example
+
+            for (int i = 20; i < 26; i++)
+            {
+                outputListbox.Items.Add("FOR LOOP ITERATION: " + i);
+            }
+        }
+    }
+}
